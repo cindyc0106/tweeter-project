ff7782e9583705d21026a7692f69de716e56501c
Cindy <cindy.c0106@gmail.com>
2022-09-13 23:06:16 +0000

    adding additional css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-  
   $('#tweet-text').on('input', onChange);
 });
 
 const onChange = function() {
-  // console.log(this)
-  const input = $(this)
-  const remaining = 140 - input.val().length
-  // let spacesLeft = 140 - input.val().length;
-  console.log(remaining)
-  const traverse = $(this).closest('form').children('div').children('.counter')
-  traverse.text(remaining);
-}
+  const input = $(this);
+  const remaining = 140 - input.val().length;
+  console.log(remaining);
+  const counter = $(this).closest('form').find('.counter');
+  counter.text(remaining);
+  if (remaining < 0) {
+    return counter.addClass('red');
+  }
+  counter.removeClass('red');
+
+};
 
 
 
