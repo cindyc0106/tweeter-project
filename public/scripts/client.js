dc11c604925c44c4637812b6bd669828bbca655d
Cindy <cindy.c0106@gmail.com>
2022-09-14 23:48:05 +0000

    error msgs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,48 +4,103 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-$(() => {  
-  renderTweets(data);
-});
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+$(() => {
 
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    for (const tweet of tweets) {
-      // calls createTweetElement for each tweet
-      const $tweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('#tweets-container').append($tweet);
+  // renderTweets(data);
+
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+
+    const $textArea = $('#tweet-text');
+    $textArea.text($textArea.val())
+    // console.log($textArea.val());
+
+    if ($textArea.val() === "") {
+      return $(".empty").slideDown(() => {
+        setTimeout(()=> {
+          $(".empty").slideUp()
+        }, 2000)
+      });
     }
+
+    if ($textArea.val().length > 140) {
+      return $(".too-much").slideDown(() => {
+        setTimeout(()=> {
+          $(".too-much").slideUp()
+        }, 2000)
+      });
+    }
+   
+    const serializedData = $('form').serialize();
+    console.log(serializedData);
+    $.post('/tweets/', serializedData, (res) => {
+      $('#tweets-container').empty();
+      loadTweets();
+      console.log(res);
+    });
+
+  });
+
+
+  const loadTweets = () => {
+
+    $.ajax({
+      type: 'GET',
+      url: '/tweets',
+      success: (data) => {
+        renderTweets(data);
+
+      }
+    });
   };
 
-  const createTweetElement = (tweet) => {
-    let $tweet = `<article class="tweet">
+  loadTweets();
+
+  ;
+
+});
+
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
+
+const renderTweets = function(tweets) {
+
+  // loops through tweets
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    const $tweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').prepend($tweet);
+
+  }
+};
+
+const createTweetElement = (tweet) => {
+  let $tweet = `<article class="tweet">
     <p class="user-icon">
     <span><img src="${tweet.user.avatars}"/>
     ${tweet.user.name} 
@@ -54,7 +109,7 @@ $(() => {
     </p>
     <div class="content">${tweet.content.text}</div>
     <footer>
-      <div>${tweet.created_at}</div>
+      <div>${timeago.format(tweet.created_at)}</div>
       <div class="icons">
         <div class="icon"><i class="fa-solid fa-flag"></i></div>
         <div ><i class="icon fa-solid fa-retweet"></i></div>
@@ -62,8 +117,9 @@ $(() => {
       </div>
     </footer>
   </article>`;
-    return $tweet;
-  };
+  return $tweet;
+};
+
 
 
 
