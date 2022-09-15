d44af4bde6e0dedc2880ddc2bf2a7daebff3c539
Cindy <cindy.c0106@gmail.com>
2022-09-15 21:30:35 +0000

    completing responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,20 +1,14 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+// Client-side 
 
 $(() => {
 
-  // renderTweets(data);
 
   $('form').on('submit', (event) => {
+
     event.preventDefault();
 
     const $textArea = $('#tweet-text');
     $textArea.text($textArea.val())
-    // console.log($textArea.val());
 
     if ($textArea.val() === "") {
       return $(".empty").slideDown(() => {
@@ -33,7 +27,7 @@ $(() => {
     }
    
     const serializedData = $('form').serialize();
-    console.log(serializedData);
+
     $.post('/tweets/', serializedData, (res) => {
       $('#tweets-container').empty();
       loadTweets();
@@ -41,7 +35,6 @@ $(() => {
     });
 
   });
-
 
   const loadTweets = () => {
 
@@ -57,35 +50,8 @@ $(() => {
 
   loadTweets();
 
-  ;
-
 });
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
 
 const renderTweets = function(tweets) {
 
@@ -95,7 +61,6 @@ const renderTweets = function(tweets) {
     const $tweet = createTweetElement(tweet);
     // takes return value and appends it to the tweets container
     $('#tweets-container').prepend($tweet);
-
   }
 };
 
